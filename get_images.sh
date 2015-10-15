4ec56c62ed32af8c5cf6e3a5cefb80cd36155bed
ibravo <ibravo@hotmail.com>
2015-10-15 16:16:58 -0400

    Create get_images.sh

---
+++ get_images.sh
@@ -0,0 +1,12 @@
+REM https://repos.fedorapeople.org/repos/openstack-m/rdo-images-centos-liberty/
+wget https://repos.fedorapeople.org/repos/openstack-m/rdo-images-centos-liberty/deploy-ramdisk-ironic.initramfs
+wget https://repos.fedorapeople.org/repos/openstack-m/rdo-images-centos-liberty/deploy-ramdisk-ironic.kernel 
+wget https://repos.fedorapeople.org/repos/openstack-m/rdo-images-centos-liberty/deploy-ramdisk-ironic.tar
+wget https://repos.fedorapeople.org/repos/openstack-m/rdo-images-centos-liberty/ironic-python-agent.initramfs
+wget https://repos.fedorapeople.org/repos/openstack-m/rdo-images-centos-liberty/ironic-python-agent.kernel
+wget https://repos.fedorapeople.org/repos/openstack-m/rdo-images-centos-liberty/ironic-python-agent.tar
+wget https://repos.fedorapeople.org/repos/openstack-m/rdo-images-centos-liberty/ironic-python-agent.vmlinuz
+wget https://repos.fedorapeople.org/repos/openstack-m/rdo-images-centos-liberty/overcloud-full.initrd
+wget https://repos.fedorapeople.org/repos/openstack-m/rdo-images-centos-liberty/overcloud-full.qcow2
+wget https://repos.fedorapeople.org/repos/openstack-m/rdo-images-centos-liberty/overcloud-full.tar
+wget https://repos.fedorapeople.org/repos/openstack-m/rdo-images-centos-liberty/overcloud-full.vmlinuz
