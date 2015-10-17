33da9993149ff7b5151500d948057ddea954335f
ibravo <ibravo@hotmail.com>
2015-10-17 14:27:34 -0400

    Update 2.install_as_stack.sh

--- 2.install_as_stack.sh
+++ 2.install_as_stack.sh
@@ -1,9 +1,10 @@
+echo git clone https://github.com/ibravo/tripleo
 #Stack User
 mkdir -p /home/stack/images
 mkdir -p /home/stack/templates
 sudo hostnamectl set-hostname bl16.hq.ltg
 sudo hostnamectl set-hostname --transient bl16.hq.ltg
 echo Modify /etc/hosts bl16.hq.ltg bl16
-sudo yum install git -y
+sudo yum install git vnstat mc htop -y
 sudo yum update -y
-echo git clone https://github.com/ibravo/tripleo
+
