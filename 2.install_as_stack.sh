c9cb90f18a5e7cdbbbd9094fe5456443f88c287a
Ignacio Bravo <ibravo@hotmail.com>
2015-10-19 16:01:08 -0400

    updated 2

--- 2.install_as_stack.sh
+++ 2.install_as_stack.sh
@@ -5,6 +5,6 @@ mkdir -p /home/stack/templates
 sudo hostnamectl set-hostname bl16.hq.ltg
 sudo hostnamectl set-hostname --transient bl16.hq.ltg
 echo Modify /etc/hosts bl16.hq.ltg bl16
-sudo yum install git vnstat mc htop -y
-sudo yum update -y
+sudo yum -y install git vnstat mc htop wget
+echo Run 'sudo yum update -y' in a different window while you continue with Step 3
 
