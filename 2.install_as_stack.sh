4ce23d7d6aef165189b0c59f357cb0380eabd2f4
Ignacio Bravo <ibravo@hotmail.com>
2015-10-24 10:33:42 -0400

    Split node config and added full script

--- 2.install_as_stack.sh
+++ 2.install_as_stack.sh
@@ -1,10 +1,15 @@
 echo git clone https://github.com/ibravo/tripleo
 #Stack User
+
+#Clone git repository
 mkdir -p /home/stack/images
 mkdir -p /home/stack/templates
-sudo hostnamectl set-hostname bl16.hq.ltg
-sudo hostnamectl set-hostname --transient bl16.hq.ltg
-echo Modify /etc/hosts bl16.hq.ltg bl16
-sudo yum -y install git vnstat mc htop wget
-echo Run 'sudo yum update -y' in a different window while you continue with Step 3
 
+sh /home/stack/tripleo/bkp.scripts/3_get_images.sh
+
+echo Now start the install
+sudo yum install -y http://rdoproject.org/repos/openstack-liberty/rdo-release-liberty.rpm
+sudo yum install -y python-tripleoclient
+
+# After finalizing
+echo when wget is done, restart and install: openstack undercloud install
---
+++ 3.install_undercloud.sh
@@ -0,0 +1,49 @@
+echo Run this command after the images have been downloaded
+cd /home/stack/images
+openstack overcloud image upload
+cd /home/stack
+
+# Deploy configuration
+# 3 Cephs       = High HDD
+# 3 Controllers = 16 Gb RAM, Std HDD
+# 2 Compute     = High CPU Count, High RAM
+# 
+# ———————————————
+# Undercloud	16
+# ———————————————
+# Ceph		7,8,9
+# Control	4,5,6
+# Compute	2,3,11
+# ———————————————
+# Used		10 - Inlingua
+# ———————————————
+# Future Use
+# oVirt Mgmt	1
+# oVirt		12,13
+# Compute	14,15
+# ———————————————
+
+# Controllers
+echo Controllers
+openstack baremetal import --json /home/stck/tripleo/files/instack.control.4-6.json
+openstack baremetal configure boot
+openstack baremetal introspection bulk start
+openstack flavor create --id auto --ram 4096 --disk 40 --vcpus 8 controller
+openstack flavor set --property "cpu_arch"="x86_64" --property "capabilities:boot_option"="local" controller
+
+# Ceph
+echo Ceph
+openstack baremetal import --json /home/stck/tripleo/files/instack.ceph.7-9.json
+openstack baremetal configure boot
+openstack baremetal introspection bulk start
+openstack flavor create --id auto --ram 4096 --disk 400 --vcpus 8 ceph
+openstack flavor set --property "cpu_arch"="x86_64" --property "capabilities:boot_option"="local" ceph
+
+# Compute
+echo Compute
+openstack baremetal import --json /home/stck/tripleo/files/instack.compute.2-3-11.json
+openstack baremetal configure boot
+openstack baremetal introspection bulk start
+openstack flavor create --id auto --ram 16384 --disk 40 --vcpus 24 compute
+openstack flavor set --property "cpu_arch"="x86_64" --property "capabilities:boot_option"="local" compute
+
