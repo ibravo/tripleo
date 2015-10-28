9cf52fe597c0fc607b3399bdd7c1f70ed0f0df0c
ibravo <ibravo@hotmail.com>
2015-10-28 09:50:50 -0400

    removed https

--- bkp.scripts/3_get_images.ltg.sh
+++ bkp.scripts/3_get_images.ltg.sh
@@ -11,14 +11,14 @@
 
 #REM https://repos.fedorapeople.org/repos/openstack-m/rdo-images-centos-liberty/
 cd /home/stack/images
-wget https://katello.hq.ltg/pulp/repos/rdo-manager/deploy-ramdisk-ironic.initramfs --no-check-certificate
-wget https://katello.hq.ltg/deploy-ramdisk-ironic.kernel --no-check-certificate
-wget https://katello.hq.ltg/deploy-ramdisk-ironic.tar --no-check-certificate
-wget https://katello.hq.ltg/ironic-python-agent.initramfs --no-check-certificate
-wget https://katello.hq.ltg/ironic-python-agent.kernel --no-check-certificate
-wget https://katello.hq.ltg/ironic-python-agent.tar --no-check-certificate
-wget https://katello.hq.ltg/ironic-python-agent.vmlinuz --no-check-certificate
-wget https://katello.hq.ltg/overcloud-full.initrd --no-check-certificate
-wget https://katello.hq.ltg/overcloud-full.qcow2 --no-check-certificate
-wget https://katello.hq.ltg/overcloud-full.tar --no-check-certificate
-wget https://katello.hq.ltg/overcloud-full.vmlinuz --no-check-certificate
+wget http://katello.hq.ltg/pulp/repos/rdo-manager/deploy-ramdisk-ironic.initramfs 
+wget http://katello.hq.ltg/deploy-ramdisk-ironic.kernel 
+wget http://katello.hq.ltg/deploy-ramdisk-ironic.tar 
+wget http://katello.hq.ltg/ironic-python-agent.initramfs 
+wget http://katello.hq.ltg/ironic-python-agent.kernel 
+wget http://katello.hq.ltg/ironic-python-agent.tar 
+wget http://katello.hq.ltg/ironic-python-agent.vmlinuz
+wget http://katello.hq.ltg/overcloud-full.initrd 
+wget http://katello.hq.ltg/overcloud-full.qcow2
+wget http://katello.hq.ltg/overcloud-full.tar 
+wget http://katello.hq.ltg/overcloud-full.vmlinuz
