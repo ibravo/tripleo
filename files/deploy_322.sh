413302855e1616c541a4675441bd029a816ea189
Ignacio Bravo <ibravo@hotmail.com>
2015-11-02 07:55:16 -0500

    Split deployment in working types

---
+++ files/deploy_322.sh
@@ -0,0 +1,11 @@
+
+openstack overcloud deploy --templates \
+--control-scale 3 --control-flavor controller \
+--compute-scale 2 --compute-flavor compute \
+--ceph-storage-scale 3 --ceph-storage-flavor ceph --block-storage-flavor ceph --swift-storage-flavor ceph \
+--libvirt-type kvm \
+-e ~/tripleo/mycloud/storage-environment.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/puppet-pacemaker.yaml \
+--ntp-server pool.ntp.org --neutron-network-type vxlan --neutron-tunnel-types vxlan \
+--timeout 90
+
