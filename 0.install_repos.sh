3175482dc414d01b73c234c2a6276aba7011d7fa
ibravo <ibravo@hotmail.com>
2016-02-19 16:18:59 -0500

    Create 0.install_repos.sh

---
+++ 0.install_repos.sh
@@ -0,0 +1,21 @@
+
+sudo curl -o /etc/yum.repos.d/delorean.repo http://trunk.rdoproject.org/centos7/current-passed-ci/delorean.repo
+sudo curl -o /etc/yum.repos.d/delorean-deps.repo http://trunk.rdoproject.org/centos7/delorean-deps.repo
+
+yum install -y yum-plugin-priorities
+
+# Install Ceph key and repo from download.ceph.com
+
+sudo rpm --import 'https://download.ceph.com/keys/release.asc'
+
+cat <<-EOF>>/etc/yum.repos.d/ceph.repo
+[ceph]
+name=Ceph packages from CEPH
+baseurl=http://download.ceph.com/rpm-infernalis/el7/x86_64/
+failovermethod=priority
+enabled=1
+gpgcheck=0
+EOF
+
+
+yum update -y
