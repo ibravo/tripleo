d36a2a20b779b9c3da76638ebfe28881a6b24b00
ibravo <ibravo@hotmail.com>
2016-04-02 13:01:09 -0400

    Update 3_build_images.sh

--- bkp.scripts/3_build_images.sh
+++ bkp.scripts/3_build_images.sh
@@ -5,7 +5,7 @@ cd /home/stack/images
 
 export NODE_DIST=centos7
 export USE_DELOREAN_TRUNK=1
-export DELOREAN_TRUNK_REPO="http://trunk.rdoproject.org/centos7/consistent"
+export DELOREAN_TRUNK_REPO="http://trunk.rdoproject.org/centos7-mitaka/current-passed-ci"
 export DELOREAN_REPO_FILE="delorean.repo"
 export DIB_YUM_REPO_CONF="/home/stack/tripleo/repos/ceph.repo"
 openstack overcloud image build --all
