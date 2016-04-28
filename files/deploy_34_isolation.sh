f6657522fecdfa261bea8a9bdf043ccb32655540
ibravo <ibravo@hotmail.com>
2016-04-28 00:41:59 -0400

    Updated Apr 28

---
+++ files/deploy_34_isolation.sh
@@ -0,0 +1,13 @@
+
+openstack overcloud deploy --templates \
+--control-scale 3 --control-flavor controller \
+--compute-scale 4 --compute-flavor compute \
+--ceph-storage-scale 3 --ceph-storage-flavor ceph --block-storage-flavor ceph --swift-storage-flavor ceph \
+--libvirt-type kvm \
+-e ~/tripleo/mycloud/storage-environment.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/puppet-pacemaker.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
+-e /home/stack/tripleo/mycloud/network-environment.yaml \
+--ntp-server pool.ntp.org --neutron-network-type vxlan --neutron-tunnel-types vxlan \
+--timeout 90
+
